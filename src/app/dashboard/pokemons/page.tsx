808a791af3032f690c15b821bfaef84f040eba16
Kevin Nieto <alexis.nieto@tigo.com.hn>
2024-02-20 18:28:47 -0600

    Inicio Pokemons

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,24 @@
+import { PokemonsReponse, SimplePokemon } from ".";
+
+const getPokemons = async(limit = 20, offset = 0): Promise <SimplePokemon[]> => {
+    const data: PokemonsReponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).then(res =>res.json())
+
+    const pokemons = data.results.map(pokemon =>({
+        id: pokemon.url.split('/').at(-2)!,
+        name: pokemon.name,
+    })  )
+    return pokemons;
+}
+export default async function PokemonsPage() {
+
+ const pokemons = await getPokemons(151);
+
+  return (
+    <div className="flex flex-col">
+        <div className="flex flex-wrap gap-10 items-center justify-center">
+
+        </div>
+      {JSON.stringify(pokemons)}
+    </div>
+  );
+}
