ba51606801e27dbff3d452e9eb258adda0665eba
Kevin Nieto <alexis.nieto@tigo.com.hn>
2024-02-22 08:20:22 -0600

    Final seccion 5

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,15 @@ const nextConfig = {
           {
             protocol: 'https',
             hostname: 'images.unsplash.com'
-          },
+          },     
+           {
+            protocol: 'https',
+            hostname: 'raw.githubusercontent.com'
+          }
     
         ]
-      }
+      },
+      
 
 
 };
